2e649a180e0dc9f540d67a23fea567f1728c218b
React <3809164@gmail.com>
2022-11-25 23:06:57 +0200

    Header_NavLink_Routes_2

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import header from "./Header.module.css"
-import {Container, Image, Nav, Navbar} from "react-bootstrap";
+import {Button, Container, Form, FormControl, Image, Nav, Navbar} from "react-bootstrap";
 import Logo from "../Img/logo.svg"
 import NavbarCollapse from "react-bootstrap/NavbarCollapse";
 import {NavLink} from "react-router-dom";
@@ -9,18 +9,35 @@ class Header extends Component {
     render() {
         return (
             <Navbar className={header.header} collapseOnSelect="md" bg={"dark"} variant={"dark"}>
-                <Container>
-                    <Navbar.Brand className={""} >
-                        <Image className={header.logo} src={Logo} alt={"logo"}/>
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls={"responsive-navbar-nav"}/>
-                    <NavbarCollapse id={"responsive-navbar-nav"}>
-                        <Nav className={"mr-auto"}>
-                            <NavLink to={"/home"} className = { navData => navData.isActive ? header.active : header.item }>Home</NavLink>
-                            <NavLink to={"/contacts"} className = { navData => navData.isActive ? header.active : header.item }>Contacts</NavLink>
-                            <NavLink to={"/about"} className = { navData => navData.isActive ? header.active : header.item }>About us</NavLink>
-                        </Nav>
-                    </NavbarCollapse>
+                <Container >
+                    <div className={header.container}>
+                        <Navbar.Brand className={""}>
+                            <Image className={header.logo} src={Logo} alt={"logo"}/>
+                        </Navbar.Brand>
+                        <Navbar.Toggle aria-controls={"responsive-navbar-nav"}/>
+                        <NavbarCollapse id={"responsive-navbar-nav"}>
+                            <Nav className={"mr-auto"}>
+                                <NavLink to={"/home"}
+                                         className={navData => navData.isActive ? header.active : header.item}>Home</NavLink>
+                                <NavLink to={"/contacts"}
+                                         className={navData => navData.isActive ? header.active : header.item}>Contacts</NavLink>
+                                <NavLink to={"/about"}
+                                         className={navData => navData.isActive ? header.active : header.item}>About
+                                    us</NavLink>
+                            </Nav>
+                            <div>
+                                <Form>
+                                    <FormControl
+                                        placeholder={"Search"}
+                                        type={"text"}
+                                    />
+                                </Form>
+                                <div>
+                                    <Button>Push</Button>
+                                </div>
+                            </div>
+                        </NavbarCollapse>
+                    </div>
                 </Container>
             </Navbar>
         );
