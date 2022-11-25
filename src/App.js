d9e3a6658e50c16aba2a89e0c3cac4e31ae6914a
React <3809164@gmail.com>
2022-11-25 15:50:47 +0200

    Header_NavLink_Rotes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import Home from "./Components/Home/Home";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Contacts from "./Components/Contacts/Contacts";
+import About from "./Components/About/About";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (<BrowserRouter>
+            <Header/>
+            <Routes>
+                <Route path="/home" element={<Home/>}/>
+                <Route path="/contacts" element={<Contacts/>}/>
+                <Route path="/about" element={<About/>}/>
+            </Routes>
+            <Footer/>
+        </BrowserRouter>);
 }
 
 export default App;
